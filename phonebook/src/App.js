d10dba3ebdd77b708faaa4c9b55e4191d62f6c5c
Joe <joedravarol@gmail.com>
2021-05-30 23:55:36 +0100

    Complete phonebook exercise step1 feat: Implement functionality to add a new person to phonebook.

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -6,18 +6,39 @@ const App = () => {
   ])
   const [newName, setNewName] = useState('')
 
+  const addPerson = (event) => {
+    event.preventDefault()
+    const newPerson = { name: newName }
+
+    setPersons( [...persons, newPerson ] )
+    setNewName('')
+  }
+
+  const handleOnChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleOnChange} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button 
+            type="submit"
+            onClick={addPerson}
+          >
+            add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
+      
+      {persons.map(({ name }) => 
+        <p key={name}>{name}</p>
+      )}
     </div>
   )
 }
