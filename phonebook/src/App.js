4f79552421f12ebc01604e3db4ff6884192b90dc
Joe <joedravarol@gmail.com>
2021-05-31 00:38:54 +0100

    Complete phonebook exercise step4 feat: Implement a search field that is used to filter the list of people by name (case insensitive).

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,13 +2,14 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterName, setFilterName] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -34,9 +35,21 @@ const App = () => {
     }
   }
 
+  const filteredPersons = persons.filter(person => {
+    const nameLower = person.name.toLowerCase()
+    const filterLower = filterName.toLowerCase()
+
+    return nameLower.includes(filterLower)
+  })
+  
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filterName} onChange={handleOnChange(setFilterName)} />
+      </div>
+
+      <h2>Add a new</h2>
       <form>
         <div>
           name: <input value={newName} onChange={handleOnChange(setNewName)} />
@@ -53,9 +66,10 @@ const App = () => {
           </button>
         </div>
       </form>
+
       <h2>Numbers</h2>
 
-      {persons.map(({ name, number }) => 
+      {filteredPersons.map(({ name, number }) => 
         <p key={name}>
           {name} {number}
         </p>
